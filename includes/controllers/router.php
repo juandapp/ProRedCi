ed11a96ced66f8159ed1df0d19c8c01cecf8c046
Gustavo Adolfo <Gustavo Adolfo@GUSTAVO-VM>
2013-05-03 17:58:47 -0500

    Controllers

--- includes/controllers/router.php
+++ includes/controllers/router.php
@@ -4,52 +4,43 @@
 $request = $_SERVER['QUERY_STRING'];
 
 // Diseccionar la pagina solicitada y las variables del get
-$parsed = explode('&' , $request);
+$parsed = explode('&', $request);
 
 // Obtener la página
 $page = array_shift($parsed);
 
 // Los siguientes argumentos del arreglo son variables enviadas por GET
 $getVars = array();
-foreach ($parsed as $argument)
-{
-	// Dividir la variables GET por medio del '=' para separar las 
-        // variables de los valores
-	list($variable , $value) = explode('=' , $argument);
-	$getVars[$variable] = $value;
+foreach ($parsed as $argument) {
+    // Dividir la variables GET por medio del '=' para separar las 
+    // variables de los valores
+    list($variable, $value) = explode('=', $argument);
+    $getVars[$variable] = $value;
 }
 
 //compute the path to the file
 $target = CONTROLLER_PATH . $page . '.php';
 
 //get target
-if (file_exists($target))
-{
-	include_once($target);
+if (file_exists($target)) {
+    include_once($target);
 
-	//modify page to fit naming convention
-	$class = ucfirst($page) . '_Controller';
+    //modify page to fit naming convention
+    $class = ucfirst($page) . '_Controller';
 
-	//instantiate the appropriate class
-	if (class_exists($class))
-	{
-		$controller = new $class;
-	}
-	else
-	{
-		//did we name our class correctly?
-		die('class does not exist!');
-	}
-}
-else
-{
-	//can't find the file in 'controllers'! 
-	die('page does not exist!');
+    //instantiate the appropriate class
+    if (class_exists($class)) {
+        $controller = new $class;
+    } else {
+        //did we name our class correctly?
+        die('class does not exist!');
+    }
+} else {
+    //can't find the file in 'controllers'! 
+    die('page does not exist!');
 }
 
 //once we have the controller instantiated, execute the default function
 //pass any GET varaibles to the main method
 $controller->main($getVars);
-
-
 ?>
