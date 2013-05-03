22e14eea0d14815cb7325c9e288ebb54e7863c83
Gustavo Adolfo Rodriguez Libreros <Gustavo.Adolfo.Rodriguez.L@gmail.com>
2013-05-03 17:14:33 -0500

    Update

---
+++ includes/config.php
@@ -0,0 +1,7 @@
+<?php
+//Configuracion de Constantes para la Base de Datos
+defined('DATABASE_HOST') ? NULL : define('DATABASE_HOST', '10.0.1.');
+defined('DATABASE_NAME') ? NULL : define('DATABASE_NAME', 'theprocesssocialnetwork');
+defined('DATABASE_USER') ? NULL : define('DATABASE_USER', 'root');
+defined('DATABASE_PASSWORD') ? NULL : define('DATABASE_PASSWORD', '');
+?>
