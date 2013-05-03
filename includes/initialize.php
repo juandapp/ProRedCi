353a84fd3bc775d50522dd9a88bb737506ffbd91
Gustavo Adolfo <Gustavo Adolfo@GUSTAVO-VM>
2013-05-03 16:26:23 -0500

    Initialize

--- includes/initialize.php
+++ includes/initialize.php
@@ -1,36 +1,12 @@
 <?php
 
-// Define the core paths
-// Define them as absolute paths to make sure that require_once works as expected
-
-// DIRECTORY_SEPARATOR is a PHP pre-defined constant
-// (\ for Windows, / for Unix)
+// Separación de los archivos dependiendo de Sistema Operativo
 defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
-defined('SITE_ROOT') ? NULL : define('SITE_ROOT', dirname(dirname(__FILE__)) . DS);        
-defined('LIB_PATH') ? null : define('LIB_PATH', SITE_ROOT.'includes'.DS);
-define('SERVER_ROOT' , '/var/www/WEB_ROOT_FOLDER');
 
+// Dirección del servidor
+defined('SERVER_ROOT') ? NULL : define('SITE_ROOT', dirname(dirname(__FILE__)) . DS);        
 
-
-defined('DATA_PATH') ? NULL : define('DATA_PATH', LIB_PATH.DS.'data' . DS);
-defined('MODEL_PATH') ? NULL : define('MODEL_PATH', LIB_PATH.DS.'model' . DS);
-defined('VIEW_PATH') ? NULL : define('VIEW_PATH', LIB_PATH.DS.'view' . DS);
-defined('CONTROLLER_PATH') ? NULL : define('CONTROLLER_PATH', LIB_PATH.DS.'controller' . DS);
-
-
-// load config file first
-require_once(LIB_PATH.'config.inc.php');
-
-// load basic functions next so that everything after can use them
-require_once(LIB_PATH.'functions.php');
-
-// load core objects
-require_once(LIB_PATH.'session.php');
-require_once(DATA_PATH.'mysqldatabase.php');
-//require_once(LIB_PATH.DS.'database_object.php');
-//
-//// load database-related classes
-require_once(MODEL_PATH.'user.php');
-require_once(MODEL_PATH.'tag.php');
+// Dirección del sitio web
+define('SITE_ROOT' , 'C:'.DS.'wamp'.DS.'www');
 
 ?>
