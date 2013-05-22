f6bda2aedc92b180ab72ec78385018f1476c8e74
VirtualMachine <VirtualMachine@VIRTUAL-MACHINE>
2013-05-22 15:40:21 -0500

    Update

--- includes/models/config.db.php
+++ includes/models/config.db.php
@@ -1,6 +1,6 @@
 <?php
 //Configuracion de Constantes para la Base de Datos
-defined('DATABASE_HOST') ? NULL : define('DATABASE_HOST', '192.168.1.101');
+defined('DATABASE_HOST') ? NULL : define('DATABASE_HOST', 'localhost');
 defined('DATABASE_NAME') ? NULL : define('DATABASE_NAME', 'proredci');
 defined('DATABASE_USER') ? NULL : define('DATABASE_USER', 'root');
 defined('DATABASE_PASSWORD') ? NULL : define('DATABASE_PASSWORD', 'cpqrunivalle');
--- public/views/login.php
+++ public/views/login.php
@@ -14,10 +14,8 @@
 
         <script>
             $(function() {
-                $("#dialog").dialog();
-            });
-
-        </script>
+            $("#dialog").dialog();
+            });        </script>
     </head>
     <body>
         <nav id="mainMenu">
@@ -25,7 +23,7 @@
 
                 <a class="navIcon" href="index.php"><img title="Index" alt="Index" src="public/images/home.png" width="22" height="22"/></a>
 
-                
+
                 <form id="loginform" class="navRightItem" action="index.php?login&singin" method="post">
                     <div class="navLeftItem">
                         <input class="upInputs" type="text" placeholder="Usuario" required="" name="username" id="username" />
@@ -52,7 +50,7 @@
             </div>
         </header>
 
-        
+
         <div id="wrapper">
             <section id="main">
 
@@ -80,7 +78,7 @@
                         <input name="busqueda" type="text" size="40" placeholder="Buscar..." />
                     </form>
                 </article>
-                
+
                 <article class="loginArticle"><!-- Tops -->
                     <h2>Top 5 de de los m&aacute;s valorados</h2>
                     <div id="accordion">
@@ -117,17 +115,6 @@
 
                     </div>
                 </article><!-- Cierre Tops -->
-                
-                
-                <aside class="loginAside">
-                    <script type="text/javascript" src="//ajax.googleapis.com/ajax/static/modules/gviz/1.0/chart.js"> {"dataSourceUrl":"//docs.google.com/spreadsheet/tq?key=0AqvBSxHJKkTzdFpvcE13bWZhbGROaEFSYlhkYWtERGc&transpose=0&headers=0&range=A1%3AB15&gid=2&pub=1","options":{"titleTextStyle":{"bold":true,"color":"#000","fontSize":"12"},"animation":{"duration":500},"colors":["#3366CC","#DC3912","#FF9900","#109618","#990099","#0099C6","#DD4477","#66AA00","#B82E2E","#316395","#994499","#22AA99","#AAAA11","#6633CC","#E67300","#8B0707","#651067","#329262","#5574A6","#3B3EAC","#B77322","#16D620","#B91383","#F4359E","#9C5935","#A9C413","#2A778D","#668D1C","#BEA413","#0C5922","#743411"],"width":300,"is3D":true,"pieSliceText":"percentage","hAxis":{"useFormatFromData":true,"minValue":null,"viewWindow":{"min":null,"max":null},"maxValue":null},"vAxes":[{"useFormatFromData":true,"minValue":null,"viewWindow":{"min":null,"max":null},"maxValue":null},{"useFormatFromData":true,"minValue":null,"viewWindow":{"min":null,"max":null},"maxValue":null}],"pieHole":0,"title":"\u00bfEn que entidad ha encontrado m\u00e1s dificultad a la hora de realizar un tr\u00e1mite?","booleanRole":"certainty","height":320,"legend":"right","tooltip":{}},"state":{},"view":{},"isDefaultVisualization":true,"chartType":"PieChart","chartName":"Chart 1"} </script>
-                </aside>
-                
-                <aside class="loginAside">
-                    <a href="https://docs.google.com/forms/d/1Rvn9HOzSfZtrVSiDFJjmCtAdFYsP8P3Vm08Akg0YwuI/viewform#start=embed">Realiza la encuesta</a>
-                </aside>
-                
-
             </section><!-- close main -->
         </div><!-- close Wrapper -->
 
