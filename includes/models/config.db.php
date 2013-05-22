65860e9438b07f1821dd8df48b963e9f549e5d82
VirtualMachine <VirtualMachine@VIRTUAL-MACHINE>
2013-05-22 15:50:19 -0500

    Update

--- includes/models/config.db.php
+++ includes/models/config.db.php
@@ -1,6 +1,6 @@
 <?php
 //Configuracion de Constantes para la Base de Datos
-defined('DATABASE_HOST') ? NULL : define('DATABASE_HOST', 'localhost');
+defined('DATABASE_HOST') ? NULL : define('DATABASE_HOST', '10.0.1.110');
 defined('DATABASE_NAME') ? NULL : define('DATABASE_NAME', 'proredci');
 defined('DATABASE_USER') ? NULL : define('DATABASE_USER', 'root');
 defined('DATABASE_PASSWORD') ? NULL : define('DATABASE_PASSWORD', 'cpqrunivalle');
